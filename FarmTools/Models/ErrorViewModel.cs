46961c80897b7392d151a255fba15de5d4e6f8f1
david <david@DAVIDGARCIA>
2022-10-23 15:13:49 +0100

    Adicionar arquivos de projeto.

---
+++ FarmTools/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FarmTools.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
